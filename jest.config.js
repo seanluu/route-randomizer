93be71aace2db58a5a2f5e0dc4156b850fb41577
seanluu <seanluu3@gmail.com>
2025-12-24 13:10:10 -0800

    Updated Jest config for frontend/backend folder structure

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^@/backend/(.*)$': '<rootDir>/backend/$1',
+    '^@/(.*)$': '<rootDir>/frontend/$1',
   },
   transformIgnorePatterns: [
     'node_modules/(?!(jest-)?react-native|@react-native|react-clone-referenced-element|@react-navigation|expo(nent)?|@expo(nent)?/.*|@expo/vector-icons|react-native-svg|expo-router)'
